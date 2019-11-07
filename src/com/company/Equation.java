70375bdb1dcdb53e9f2e571eb50a30f86fb5ba5f
revanfall <45764065+revanfall@users.noreply.github.com>
2019-11-07 17:06:23 +0200

    Update Equation.java

--- src/com/company/Equation.java
+++ src/com/company/Equation.java
@@ -1,14 +1,24 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Equation {
-    void countEquation(int a,int b,int c){
-        double dis=Math.pow(b,2)-(4*a*c);
-        System.out.println(dis);
-        double x1=(-b+Math.sqrt(dis))/(2*a);
-        double x2=(-b-Math.sqrt(dis))/(2*a);
-        System.out.println(a+"x1 + "+b+"x2+ "+c+"=0");
-        System.out.println("x1= "+x1 +" x2="+x2);
+    void countEquation(double a,double b,double c) {
+        Double x1 = (double) 0;
+        Double x2 = (double) 0;
+        double dis = Math.pow(b, 2) - (4 * a * c);
+        if (dis > 0) {
+            x1 = (-b + Math.sqrt(dis)) / (2 * a);
+            x2 = (-b - Math.sqrt(dis)) / (2 * a);
+        } else if (dis == 0) {
+            x1 = -b / (2 * a);
+        } else if (dis < 0) {
+            x1 = null;
+            x2 = null;
+        }
+        if (dis == 0) {
+            System.out.println(x1);
+        } else {
+            System.out.println("x1= " + x1 + " x2=" + x2);
+        }
     }
 }
